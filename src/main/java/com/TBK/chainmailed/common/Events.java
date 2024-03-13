918b0e0010dbeb21a473053cdcc189b6f2415537
AstianBK <Agustinmarin2016@gmail.com>
2024-03-13 02:25:47 -0300

    Se arreglo las estadisticas.

--- src/main/java/com/TBK/chainmailed/common/Events.java
+++ src/main/java/com/TBK/chainmailed/common/Events.java
@@ -11,16 +11,22 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraftforge.event.ItemAttributeModifierEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.UUID;
+
 @Mod.EventBusSubscriber
 public class Events {
+    private static final UUID[] ARMOR_MODIFIER_UUID_PER_SLOT = new UUID[]{UUID.fromString("845DB27C-C624-495F-8C9F-6020A9A58B6B"), UUID.fromString("D8499B04-0E66-4726-AB29-64469D734E0D"), UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"), UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150")};
     public static final Attribute IMPACT_RESISTANCE = new RangedAttribute("attribute.impact_resistance",0.0d,-Double.MAX_VALUE, Double.MAX_VALUE);
     @SubscribeEvent
     public static void RightClickOnItem(PlayerInteractEvent.RightClickItem event){
@@ -49,6 +55,23 @@ public class Events {
     }
 
     @SubscribeEvent
+    public static void onItemAttribute(ItemAttributeModifierEvent event){
+        ItemStack stack = event.getItemStack();
+        EquipmentSlot slot = event.getSlotType();
+        if(stack.getItem() instanceof ArmorItem armorItem){
+            int defense=armorItem.getDefense();
+            UUID uuid = ARMOR_MODIFIER_UUID_PER_SLOT[slot.getIndex()];
+            int cc = ((ArmorItem)stack.getItem()).getMaterial()==ArmorMaterials.CHAIN ? getDefenseBonusForSlot(slot) : defense+getDefenseBonusForSlot(slot);
+            if((((IReinforcedChain)armorItem).hasChainmailed(stack.getOrCreateTag()) || ((ArmorItem)stack.getItem()).getMaterial()==ArmorMaterials.CHAIN)
+                    && slot==armorItem.getSlot()){
+                event.removeAttribute(Attributes.ARMOR);
+                event.addModifier(Attributes.ARMOR, new AttributeModifier(uuid, "Armor modifier", cc, AttributeModifier.Operation.ADDITION));
+                event.addModifier(Events.IMPACT_RESISTANCE,new AttributeModifier(uuid,"Impact Resistance",BKConfig.impactResistanceValueChainmailedBasic,AttributeModifier.Operation.ADDITION));
+            }
+        }
+    }
+
+    @SubscribeEvent
     public static void onHurtEvent(LivingHurtEvent event) {
         LivingEntity victim = event.getEntity();
         if (victim.getAttribute(Events.IMPACT_RESISTANCE) != null) {
@@ -67,5 +90,9 @@ public class Events {
                 }
             }
         }
+    }
+
+    public static int getDefenseBonusForSlot(EquipmentSlot slot){
+        return BKConfig.armorForSlotChainmailedBasic[slot.getIndex()];
     }
 }
--- src/main/java/com/TBK/chainmailed/common/mixins/ArmorItemMixin.java
+++ src/main/java/com/TBK/chainmailed/common/mixins/ArmorItemMixin.java
@@ -29,20 +29,6 @@ import java.util.UUID;
 @Mixin(ArmorItem.class)
 public abstract class ArmorItemMixin extends Item implements IReinforcedChain {
 
-    @Shadow public abstract InteractionResultHolder<ItemStack> use(Level p_40395_, Player p_40396_, InteractionHand p_40397_);
-
-    @Shadow @Final private float toughness;
-
-    @Shadow @Final protected float knockbackResistance;
-
-    @Shadow @Final private int defense;
-    @Shadow @Final private static UUID[] ARMOR_MODIFIER_UUID_PER_SLOT;
-    @Shadow @Final protected EquipmentSlot slot;
-
-    @Shadow public abstract EquipmentSlot getSlot();
-
-    private final double slash_resistance=0.5D;
-
     public ArmorItemMixin(Properties p_41383_) {
         super(p_41383_);
     }
@@ -55,24 +41,6 @@ public abstract class ArmorItemMixin extends Item implements IReinforcedChain {
         return !ItemStack.of(tag.getCompound("reinforcedChain")).isEmpty();
     }
 
-    @Override
-    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
-        int cc = ((ArmorItem)stack.getItem()).getMaterial()==ArmorMaterials.CHAIN ? this.getDefenseBonusForSlot(slot) : this.defense+this.getDefenseBonusForSlot(slot);
-        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
-        UUID uuid = ARMOR_MODIFIER_UUID_PER_SLOT[slot.getIndex()];
-        builder.put(Attributes.ARMOR, new AttributeModifier(uuid, "armor modifier", cc, AttributeModifier.Operation.ADDITION));
-        builder.put(Attributes.ARMOR_TOUGHNESS, new AttributeModifier(uuid, "Armor toughness", (double)this.toughness, AttributeModifier.Operation.ADDITION));
-        builder.put(Events.IMPACT_RESISTANCE,new AttributeModifier(uuid,"Impact Resist",BKConfig.impactResistanceValueChainmailedBasic, AttributeModifier.Operation.ADDITION));
-        if (this.knockbackResistance > 0) {
-            builder.put(Attributes.KNOCKBACK_RESISTANCE, new AttributeModifier(uuid, "Armor knockback resistance", (double)this.knockbackResistance, AttributeModifier.Operation.ADDITION));
-        }
-        if((hasChainmailed(stack.getOrCreateTag()) || ((ArmorItem)stack.getItem()).getMaterial()==ArmorMaterials.CHAIN) && slot==this.getSlot() ){
-            return builder.build();
-        }else {
-            return super.getAttributeModifiers(slot, stack);
-        }
-    }
-
 
     @Override
     public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> p_41423_, TooltipFlag p_41424_) {
@@ -80,9 +48,5 @@ public abstract class ArmorItemMixin extends Item implements IReinforcedChain {
         if(hasChainmailed(p_41421_.getOrCreateTag())){
             p_41423_.add(Component.translatable("armoritem.chainmailed.has_chainmailed").withStyle(ChatFormatting.GRAY));
         }
-    }
-
-    public int getDefenseBonusForSlot(EquipmentSlot slot){
-        return BKConfig.armorForSlotChainmailedBasic[slot.getIndex()];
     }
 }
